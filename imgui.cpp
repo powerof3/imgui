d7645d94fdc1a6e25a359f7a3382989ff48f8a1b
powerof3 <powerof3@users.noreply.github.com>
2025-11-01 13:46:12 +0530

    Scale text shadow offset

--- imgui.cpp
+++ imgui.cpp
@@ -1504,6 +1504,7 @@ void ImGuiStyle::ScaleAllSizes(float scale_factor)
     SeparatorTextPadding = ImTrunc(SeparatorTextPadding * scale_factor);
     DisplayWindowPadding = ImTrunc(DisplayWindowPadding * scale_factor);
     DisplaySafeAreaPadding = ImTrunc(DisplaySafeAreaPadding * scale_factor);
+    TextShadowOffset = ImTrunc(TextShadowOffset * scale_factor);
     MouseCursorScale = ImTrunc(MouseCursorScale * scale_factor);
 }
 
